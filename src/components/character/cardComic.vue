677b709aabb4694bc77aece55ede192619f771d2
gabriel-roque <gabrielrms.dll@gmail.com>
2019-12-31 14:36:22 -0300

    [ADD] Profile view and Card Comic

---
+++ src/components/character/cardComic.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card
+      class="mx-auto"
+      max-width="220"
+      light
+  >
+    <v-img
+        :src="preview.path + '.' + preview.extension"
+        height="300px"
+    />
+
+    <v-card-title>
+      {{this.title}}
+    </v-card-title>
+
+    <v-card-actions class="mt-0">
+        <v-btn
+            color="red"
+            dark
+            small
+            :href="this.detailLink"
+            target="_blank"
+        >
+          More details
+        </v-btn>
+
+    </v-card-actions>
+
+  </v-card>
+</template>
+
+<script>
+    export default {
+        name: "cardComic",
+        props: {
+            preview: Object,
+            title: String,
+            detailLink: String
+        }
+    }
+</script>
---
+++ src/views/character/ProfileHero.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-content>
+    <v-container>
+      <app-bar/>
+      <v-card class="marvel-pattern elevation-4">
+        <v-row justify="start">
+          <v-col cols="12" align="center">
+            <v-avatar
+                width="150"
+                height="150"
+                class="elevation-4">
+              <v-img
+                  :src="hero.thumbnail.path + '.' + hero.thumbnail.extension"
+              />
+            </v-avatar>
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-col cols="12" align="center">
+            <p class="display-1 font-weight-bold">{{hero.name}}</p>
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-col xl="8" lg="10" md="10" >
+            <p class="pl-4 pr-4 ">{{hero.description}}</p>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-container>
+
+    <v-container style="width: 80%">
+      <v-card class="elevation-4 pa-5">
+        <v-row justify="center">
+          <v-col
+              xl="2"
+              align="center"
+              v-for="comic in comics">
+            <card-comic
+                :title="comic.title"
+                :preview="comic.thumbnail"
+                :detailLink="comic.urls[0].url"
+            />
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-container>
+  </v-content>
+
+</template>
+
+<script>
+    import {mapGetters, mapActions} from "vuex";
+    import CardComic from "@/components/character/cardComic";
+    import AppBar from "@/components/core/appBar";
+
+    export default {
+        name: "ProfileHero",
+        components: {AppBar, CardComic},
+        computed: {
+            ...mapGetters({
+                hero: 'character/hero',
+                comics: 'character/comics'
+            }),
+        },
+
+        methods: {
+            ...mapActions({
+                searchComics: 'character/searchComicsHero'
+            })
+        },
+
+        watch: {
+            hero(hero) {
+                this.searchComics(hero.id)
+            }
+        }
+
+    }
+</script>
+
+<style>
+  .marvel-pattern {
+    background-size: cover;
+    background-image: url('~@/assets/img/pattern-marvel.jpg');
+  }
+
+  button:focus {
+    outline: 0;
+  }
+</style>
