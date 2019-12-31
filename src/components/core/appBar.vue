035ace12448358c773fdf8be97b73b84be9e5984
gabriel-roque <gabrielrms.dll@gmail.com>
2019-12-31 14:37:13 -0300

    [ADD] Component Core AppBar

---
+++ src/components/core/appBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar
+      app
+      clipped-left
+      color="#ed1d24"
+  >
+    <v-img
+        src="@/assets/img/marvel-logo.png"
+        max-width="90"
+    />
+    <button
+        @click="$router.push({name: 'heroes.search'})"
+        v-if="$route.name !== 'heroes.search'"
+    >
+      <i class="fa fa-search ml-5"/> Search again
+    </button>
+  </v-app-bar>
+</template>
+
+<script>
+    export default {
+        name: "appBar"
+    }
+</script>
